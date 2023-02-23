3047ca44ce6d0eb70e400992331bcb1c429c5283
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2023-02-23 14:04:25 -0300

    Adição de dois componente com lazy-loading afim de testes na integração com o serviço amplify da aws

--- test-aws/src/app/app-routing.module.ts
+++ test-aws/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home"
+  },
+  {
+    path: "home",
+    loadChildren: () => import('./../app/components/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: "page1",
+    loadChildren: () => import('./../app/components/page1/page1.module').then(m => m.Page1Module)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- test-aws/src/app/app.module.ts
+++ test-aws/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page1Component } from './components/page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
